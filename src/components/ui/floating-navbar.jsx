7b35b99c224a31dad7c4cf82d697a9402f81310b
JunaidSaleem-code <chmjunaidsaleem@gmail.com>
2025-06-10 18:26:36 +0500

    `Refactor FloatingNav component: update styles and remove unused dependencies`

--- src/components/ui/floating-navbar.jsx
+++ src/components/ui/floating-navbar.jsx
@@ -1,51 +1,28 @@
-import  { useState } from "react";
-import {
-  motion,
-  AnimatePresence,
-  useScroll,
-  useMotionValueEvent,
-} from "framer-motion";
-
 // Utility function to combine classes
 function cn(...classes) {
   return classes.filter(Boolean).join(" ");
 }
 
 export const FloatingNav = ({ navItems, className }) => {
-  const { scrollYProgress } = useScroll();
-  const [visible, setVisible] = useState(false);
-
-  useMotionValueEvent(scrollYProgress, "change", (current) => {
-    if (typeof current === "number") {
-      const previous = scrollYProgress.getPrevious();
-      const direction = current - (previous || 0);
-
-      if (scrollYProgress.get() < 0.05) {
-        setVisible(false);
-      } else {
-        setVisible(direction < 0);
-      }
-    }
-  });
 
   return (
-    
+   
       <div
-  className={cn(
-    "flex max-w-fit mt-10 inset-x-0 mx-auto border rounded-full shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[5000] px-10 py-5 items-center justify-center space-x-4 bg-black border-white",
-    className
-  )}
->
+        className={cn(
+          "flex max-w-fit fixed top-10 inset-x-0 mx-auto border rounded-full shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[5000] px-10 py-5 items-center justify-center space-x-4 border-sky-950/[0.6] bg-black/[0.6] backdrop-blur-sm border-sky-100",
+          className
+        )}
+      >
         {navItems.map((navItem, idx) => (
           <a
             key={`link-${idx}`}
             href={navItem.link}
-            className="relative dark:text-neutral-50 items-center flex space-x-1 text-neutral-600 hover:text-neutral-300"
+            className="relative dark:text-neutral-50 items-center flex space-x-1 text-neutral-600 dark:hover:text-neutral-300 hover:text-neutral-500"
           >
             <span className="block sm:hidden">{navItem.icon}</span>
             <span className="!cursor-pointer text-sm">{navItem.name}</span>
           </a>
         ))}
-      </div>
+        </div>
   );
 };
